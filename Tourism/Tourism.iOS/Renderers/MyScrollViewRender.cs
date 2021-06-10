d6f04ecf1e2faeac181271aa0cfaba242b279c93
Sylvester Eze <seze@eminenttechnology.com>
2021-06-10 01:30:54 +0100

    Works for iOS

---
+++ Tourism/Tourism.iOS/Renderers/MyScrollViewRender.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Tourism;
+using Tourism.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(myScrollView), typeof(MyScrollViewRender))]
+namespace Tourism.iOS
+{
+    public class MyScrollViewRender: ScrollViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || Element == null)
+            {
+                return;
+            }
+            if (UIDevice.CurrentDevice.CheckSystemVersion(11, 0))
+            {
+                this.ContentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentBehavior.Never;
+            }
+        }
+    }
+}
---
+++ Tourism/Tourism/CustomControls/myScrollView.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Tourism
+{
+    public class myScrollView: ScrollView
+    {
+    }
+}
